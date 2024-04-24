252ef197c6201209144343fb9ba0a8b80d254c13
Andrea Bizzotto <codewithandrea@gmail.com>
2024-04-24 13:53:22 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_ship_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/codewithandrea/flutter_ship_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codewithandrea.flutter_ship_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
