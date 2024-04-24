dba71b21d7d97baced2a4aada2925e39815f391e
Andrea Bizzotto <codewithandrea@gmail.com>
2024-04-24 13:57:50 +0100

    Add app colors & sizes, flex_color_scheme

---
+++ lib/src/constants/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // https://rydmike.com/flexcolorscheme/themesplayground-latest/
+  // Bahama Blue colors:
+  static const primary = Color(0xFF285C99);
+  static const heart = Color(0xFFDD520F);
+  static const error = Color(0xFFB1384E);
+
+  static Color greyShade200(bool isLight) =>
+      isLight ? Colors.grey.shade200 : Colors.grey.shade900;
+  static Color greyShade300(bool isLight) =>
+      isLight ? Colors.grey.shade300 : Colors.grey.shade800;
+  static Color greyShade400(bool isLight) =>
+      isLight ? Colors.grey.shade400 : Colors.grey.shade700;
+  static Color shimmerHighlightColor(bool isLight) =>
+      isLight ? Colors.grey.shade100 : Colors.grey.shade900;
+
+  static const flexScheme = FlexScheme.bahamaBlue;
+}
