d20a1af62f9c00db33fc08ecdc316dc992cdd4a1
Andrea Bizzotto <codewithandrea@gmail.com>
2024-04-25 15:17:08 +0100

    Set elevatedButtonTheme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_ship_app/src/constants/app_colors.dart';
+import 'package:flutter_ship_app/src/constants/app_sizes.dart';
 import 'package:flutter_ship_app/src/data/app_startup.dart';
 import 'package:flutter_ship_app/src/data/shared_preferences.dart';
 import 'package:flutter_ship_app/src/presentation/apps_list_screen.dart';
@@ -35,6 +36,16 @@ class MainApp extends StatelessWidget {
           bodyLarge: light.textTheme.bodyLarge?.copyWith(fontSize: 20.0),
           bodyMedium: light.textTheme.bodyMedium?.copyWith(fontSize: 18.0),
           bodySmall: light.textTheme.bodySmall?.copyWith(fontSize: 14.0),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              const EdgeInsets.all(Sizes.p8),
+            ),
+            textStyle: MaterialStateProperty.all<TextStyle>(
+              const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+            ),
+          ),
         ),
       ),
       darkTheme: FlexThemeData.dark(scheme: AppColors.flexScheme),
