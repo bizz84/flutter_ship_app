9136ed5ba2b8322106198a0af9c5d8e1313853b4
Andrea Bizzotto <codewithandrea@gmail.com>
2024-04-29 14:21:38 +0100

    Made app work on web
    Code based on this example: https://github.com/simolus3/drift/tree/develop/examples/app

--- lib/src/data/app_database.dart
+++ lib/src/data/app_database.dart
@@ -1,12 +1,8 @@
-import 'dart:io';
-
 import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as p;
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
-import 'package:sqlite3/sqlite3.dart';
+// Conditional import implementation based on the Drift Flutter web example:
+// https://github.com/simolus3/drift/tree/develop/examples/app
+import 'connection/connection.dart' as impl;
 
 part 'app_database.g.dart';
 
@@ -58,37 +54,17 @@ class TaskStatusesTable extends Table {
       ['UNIQUE (project_id, epic_id, task_id)'];
 }
 
-@DriftDatabase(
-    tables: [AppProjectsTable, EpicsTable, TasksTable, TaskStatusesTable])
+@DriftDatabase(tables: [
+  AppProjectsTable,
+  EpicsTable,
+  TasksTable,
+  TaskStatusesTable,
+])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase() : super(_openConnection());
+  AppDatabase() : super(impl.connect());
 
   @override
   int get schemaVersion => 1;
-
-  static LazyDatabase _openConnection() {
-    // the LazyDatabase util lets us find the right location for the file async.
-    return LazyDatabase(() async {
-      // put the database file, called tips.sqlite here, into the documents folder
-      // for your app.
-      final dbFolder = await getApplicationDocumentsDirectory();
-      final file = File(p.join(dbFolder.path, 'db.sqlite'));
-
-      // Also work around limitations on old Android versions
-      if (Platform.isAndroid) {
-        await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
-      }
-
-      // Make sqlite3 pick a more suitable location for temporary files - the
-      // one from the system may be inaccessible due to sandboxing.
-      final cachebase = (await getTemporaryDirectory()).path;
-      // We can't access /tmp on Android, which sqlite3 would try by default.
-      // Explicitly tell it about the correct temporary directory.
-      sqlite3.tempDirectory = cachebase;
-
-      return NativeDatabase.createInBackground(file);
-    });
-  }
 }
 
 @Riverpod(keepAlive: true)
