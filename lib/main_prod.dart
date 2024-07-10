f8150124581d81333d445f63838cb0468eb8aaf0
Andrea Bizzotto <codewithandrea@gmail.com>
2024-07-10 16:38:05 +0100

    Install firebase_core 3.6.0, update build.gradle, Podfile, initialize Firebase
    # Conflicts:
    #	pubspec.yaml
    
    # Conflicts:
    #	pubspec.yaml

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,6 +1,7 @@
+import 'firebase_options_prod.dart';
 import 'main.dart';
 
 // * Entry point for the prod flavor
 void main() async {
-  await runMainApp();
+  await runMainApp(firebaseOptions: DefaultFirebaseOptions.currentPlatform);
 }
