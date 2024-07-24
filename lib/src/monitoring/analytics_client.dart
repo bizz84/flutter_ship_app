ad0405e49365dfe868413f9ea93785c2a795af4d
Andrea Bizzotto <codewithandrea@gmail.com>
2024-07-24 09:41:58 +0100

    Add AnalyticsClient, LoggerAnalyticsClient, AnalyticsFacade
    # Conflicts:
    #	lib/main.dart

---
+++ lib/src/monitoring/analytics_client.dart
@@ -0,0 +1,9 @@
+abstract class AnalyticsClient {
+  Future<void> trackAppOpened();
+  Future<void> trackNewAppOnboarding();
+  Future<void> trackNewAppHome();
+  Future<void> trackAppCreated();
+  Future<void> trackAppUpdated();
+  Future<void> trackAppDeleted();
+  Future<void> trackTaskCompleted(int completedCount);
+}
