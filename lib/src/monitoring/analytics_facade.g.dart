1f64b4778b5deae579c71ef33c1682016d57eb7f
Andrea Bizzotto <codewithandrea@gmail.com>
2024-07-24 09:41:58 +0100

    Add AnalyticsClient, LoggerAnalyticsClient, AnalyticsFacade
    # Conflicts:
    #	lib/main.dart

---
+++ lib/src/monitoring/analytics_facade.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'analytics_facade.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$analyticsFacadeHash() => r'eadb23e9a555ef2d84fad6a27a11353ef766dfe9';
+
+/// See also [analyticsFacade].
+@ProviderFor(analyticsFacade)
+final analyticsFacadeProvider = Provider<AnalyticsFacade>.internal(
+  analyticsFacade,
+  name: r'analyticsFacadeProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$analyticsFacadeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AnalyticsFacadeRef = ProviderRef<AnalyticsFacade>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
