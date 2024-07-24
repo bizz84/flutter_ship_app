b75567bf96c5784df5a61733c4cd814a3bbd1510
Andrea Bizzotto <codewithandrea@gmail.com>
2024-07-24 09:41:58 +0100

    Add AnalyticsClient, LoggerAnalyticsClient, AnalyticsFacade
    # Conflicts:
    #	lib/main.dart

---
+++ lib/src/monitoring/logger_analytics_client.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter_ship_app/src/monitoring/analytics_client.dart';
+
+class LoggerAnalyticsClient implements AnalyticsClient {
+  const LoggerAnalyticsClient();
+
+  static const _name = 'Event';
+
+  @override
+  Future<void> trackAppOpened() async {
+    log('trackAppOpened', name: _name);
+  }
+
+  @override
+  Future<void> trackNewAppHome() async {
+    log('trackNewAppHome', name: _name);
+  }
+
+  @override
+  Future<void> trackNewAppOnboarding() async {
+    log('trackNewAppOnboarding', name: _name);
+  }
+
+  @override
+  Future<void> trackAppCreated() async {
+    log('trackAppCreated', name: _name);
+  }
+
+  @override
+  Future<void> trackAppUpdated() async {
+    log('trackAppUpdated', name: _name);
+  }
+
+  @override
+  Future<void> trackAppDeleted() async {
+    log('trackAppDeleted', name: _name);
+  }
+
+  @override
+  Future<void> trackTaskCompleted(int completedCount) async {
+    log('trackTaskCompleted(completedCount: $completedCount)', name: _name);
+  }
+}
