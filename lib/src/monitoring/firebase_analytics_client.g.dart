1291ddd54999503d75d158beca42cd5f6d70b95a
Andrea Bizzotto <codewithandrea@gmail.com>
2024-07-30 11:32:09 +0100

    Add FirebaseAnalyticsClient, update analyticsFacadeProvider
    # Conflicts:
    #	lib/src/monitoring/analytics_facade.dart
    #	lib/src/monitoring/analytics_facade.g.dart
    
    # Conflicts:
    #	lib/src/monitoring/analytics_facade.g.dart

--- lib/src/monitoring/firebase_analytics_client.g.dart
+++ lib/src/monitoring/firebase_analytics_client.g.dart
@@ -1,27 +1,29 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'analytics_facade.dart';
+part of 'firebase_analytics_client.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$analyticsFacadeHash() => r'1dad4a58b2d8287648cf4d0649eedd0c1eb7335a';
+String _$firebaseAnalyticsClientHash() =>
+    r'3250ca105921d5cccca3c7919ef92b437c2c2686';
 
-/// See also [analyticsFacade].
-@ProviderFor(analyticsFacade)
-final analyticsFacadeProvider = Provider<AnalyticsFacade>.internal(
-  analyticsFacade,
-  name: r'analyticsFacadeProvider',
+/// See also [firebaseAnalyticsClient].
+@ProviderFor(firebaseAnalyticsClient)
+final firebaseAnalyticsClientProvider =
+    Provider<FirebaseAnalyticsClient>.internal(
+  firebaseAnalyticsClient,
+  name: r'firebaseAnalyticsClientProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$analyticsFacadeHash,
+      : _$firebaseAnalyticsClientHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-typedef AnalyticsFacadeRef = ProviderRef<AnalyticsFacade>;
+typedef FirebaseAnalyticsClientRef = ProviderRef<FirebaseAnalyticsClient>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
