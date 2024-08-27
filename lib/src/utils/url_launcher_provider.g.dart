6b41d21502413bb33f8352818b2300ded934ab6a
Andrea Bizzotto <codewithandrea@gmail.com>
2024-08-27 12:17:59 +0100

    Add url_launcher, force_update_helper, use it in main.dart
    # Conflicts:
    #	android/app/src/main/AndroidManifest.xml
    
    # Conflicts:
    #	pubspec.lock
    #	pubspec.yaml
    
    # Conflicts:
    #	pubspec.yaml
    
    # Conflicts:
    #	ios/Podfile.lock
    #	pubspec.lock
    #	pubspec.yaml

---
+++ lib/src/utils/url_launcher_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'url_launcher_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$urlLauncherHash() => r'2b29da11964b1a5bff1a34bd129d36ce913aef20';
+
+/// See also [urlLauncher].
+@ProviderFor(urlLauncher)
+final urlLauncherProvider = AutoDisposeProvider<UrlLauncher>.internal(
+  urlLauncher,
+  name: r'urlLauncherProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$urlLauncherHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef UrlLauncherRef = AutoDisposeProviderRef<UrlLauncher>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
