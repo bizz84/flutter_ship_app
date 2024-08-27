2b9ea380e6f888b0b456b2df8dc57a141976a601
Andrea Bizzotto <codewithandrea@gmail.com>
2024-08-27 14:15:58 +0100

    Add firebase_remote_config 5.3.1, use it in main.dart
    # Conflicts:
    #	ios/Podfile.lock
    #	pubspec.lock
    #	pubspec.yaml
    
    # Conflicts:
    #	ios/Podfile.lock
    #	pubspec.yaml
    
    # Conflicts:
    #	ios/Podfile.lock
    #	pubspec.lock
    #	pubspec.yaml
    
    # Conflicts:
    #	ios/Podfile.lock
    #	pubspec.lock
    #	pubspec.yaml
    
    # Conflicts:
    #	ios/Podfile.lock
    #	lib/main.dart
    #	pubspec.lock
    #	pubspec.yaml
    
    # Conflicts:
    #	lib/main.dart
    #	pubspec.lock

---
+++ lib/src/utils/firebase_remote_config_provider.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_remote_config_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseRemoteConfigHash() =>
+    r'51bf4729b7e37fa9d48876d3ffb5cbc22528f7d5';
+
+/// See also [firebaseRemoteConfig].
+@ProviderFor(firebaseRemoteConfig)
+final firebaseRemoteConfigProvider =
+    FutureProvider<FirebaseRemoteConfig>.internal(
+  firebaseRemoteConfig,
+  name: r'firebaseRemoteConfigProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseRemoteConfigHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FirebaseRemoteConfigRef = FutureProviderRef<FirebaseRemoteConfig>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
