011e24bc8f560687605d6034d0f3e1da189e465c
Andrea Bizzotto <codewithandrea@gmail.com>
2024-09-24 09:31:54 +0100

    Add in_app_review: 2.0.9, provider
    # Conflicts:
    #	ios/Podfile.lock

---
+++ lib/src/utils/in_app_review_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'in_app_review_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$inAppReviewHash() => r'7cec7f9ba49319989637c4c0551d0a7d5dd41b8c';
+
+/// See also [inAppReview].
+@ProviderFor(inAppReview)
+final inAppReviewProvider = AutoDisposeProvider<InAppReview>.internal(
+  inAppReview,
+  name: r'inAppReviewProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$inAppReviewHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef InAppReviewRef = AutoDisposeProviderRef<InAppReview>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
