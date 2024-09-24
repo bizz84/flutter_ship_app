22364fed618e29d30dd7ddfcacadef03fb67a877
Andrea Bizzotto <codewithandrea@gmail.com>
2024-09-24 10:04:03 +0100

    Add basic InAppRatingService and use it in TasksChecklistController

---
+++ lib/src/data/in_app_rating_service.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'in_app_rating_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$inAppRatingServiceHash() =>
+    r'615f77006b6771083798d25fd728e3adc7cfa246';
+
+/// See also [inAppRatingService].
+@ProviderFor(inAppRatingService)
+final inAppRatingServiceProvider =
+    AutoDisposeProvider<InAppRatingService>.internal(
+  inAppRatingService,
+  name: r'inAppRatingServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$inAppRatingServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef InAppRatingServiceRef = AutoDisposeProviderRef<InAppRatingService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
