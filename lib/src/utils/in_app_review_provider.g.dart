97efdb8249ef6d1db7e23fa207bc7187b778cbb7
Andrea Bizzotto <codewithandrea@gmail.com>
2024-09-24 09:31:54 +0100

    Add in_app_review: 2.0.9, provider
    # Conflicts:
    #	ios/Podfile.lock
    
    # Conflicts:
    #	ios/Podfile.lock
    #	pubspec.lock
    #	pubspec.yaml

---
+++ lib/src/utils/in_app_review_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'in_app_review_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$inAppReviewHash() => r'1c7c3ee2be93e5001b5e740a9839b2c46be0e0c6';
+
+/// See also [inAppReview].
+@ProviderFor(inAppReview)
+final inAppReviewProvider = AutoDisposeProvider<InAppReview>.internal(
+  inAppReview,
+  name: r'inAppReviewProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$inAppReviewHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef InAppReviewRef = AutoDisposeProviderRef<InAppReview>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
