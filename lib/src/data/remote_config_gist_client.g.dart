e266cc34d844162ddf5fc5abc68c3d6162847006
Andrea Bizzotto <codewithandrea@gmail.com>
2024-10-07 12:46:14 +0100

    Fetch remote config from a GitHub gist
    # Conflicts:
    #	lib/main.dart

---
+++ lib/src/data/remote_config_gist_client.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'remote_config_gist_client.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$remoteConfigGistClientHash() =>
+    r'3bfa4d8f87d3c5bdc9a1a8acd5d45a39800a6d87';
+
+/// See also [remoteConfigGistClient].
+@ProviderFor(remoteConfigGistClient)
+final remoteConfigGistClientProvider =
+    Provider<RemoteConfigGistClient>.internal(
+  remoteConfigGistClient,
+  name: r'remoteConfigGistClientProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$remoteConfigGistClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RemoteConfigGistClientRef = ProviderRef<RemoteConfigGistClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
