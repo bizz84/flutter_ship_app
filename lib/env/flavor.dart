1341bba359444deb86e8c9609afb7702349dc2c0
Andrea Bizzotto <codewithandrea@gmail.com>
2024-10-21 09:41:15 +0100

    Removed special flavors handling for Flutter web

--- lib/env/flavor.dart
+++ lib/env/flavor.dart
@@ -1,14 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 enum Flavor { dev, stg, prod }
 
 /// Global function to return the current flavor
-/// Always returns Flavor.prod on Flutter web release builds
 Flavor getFlavor() {
-  if (kIsWeb && kReleaseMode) {
-    return Flavor.prod; // --flavor is not supported on web
-  }
   return switch (appFlavor) {
     'prod' => Flavor.prod,
     'stg' => Flavor.stg,
