ed19ac1fd99e082712d35d0cb17f038626ada19c
Andrea Bizzotto <codewithandrea@gmail.com>
2024-11-04 13:01:10 +0000

    Add web support to flavor.dart

--- lib/env/flavor.dart
+++ lib/env/flavor.dart
@@ -1,15 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 enum Flavor { dev, stg, prod }
 
 /// Global function to return the current flavor
 Flavor getFlavor() {
-  return switch (appFlavor) {
+  // * On iOS/Android, appFlavor is supported and set with the --flavor option
+  // * On web, appFlavor is not supported so we read a separate WEB_FLAVOR
+  // * variable and set it with --dart-define WEB_FLAVOR=dev|stg|prod
+  const webFlavor = String.fromEnvironment('WEB_FLAVOR');
+  const flavor = kIsWeb ? webFlavor : appFlavor;
+  return switch (flavor) {
     'prod' => Flavor.prod,
     'stg' => Flavor.stg,
     'dev' => Flavor.dev,
-    null => Flavor.dev, // * if not specified, default to dev
-    _ => throw UnsupportedError('Invalid flavor: $appFlavor'),
+    null || '' => Flavor.dev, // * if not specified or empty, default to dev
+    _ => throw UnsupportedError('Invalid flavor: $flavor'),
   };
 }
 
