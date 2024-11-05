0ffd73e94931854df831206e5754b8f39534d22f
Andrea Bizzotto <codewithandrea@gmail.com>
2024-11-05 11:43:07 +0000

    Install firebase_core 3.10.1, update settings.gradle and Podfile, initialize Firebase
    # Conflicts:
    #	ios/Podfile.lock

--- lib/main_stg.dart
+++ lib/main_stg.dart
@@ -1,6 +1,7 @@
+import 'firebase_options_stg.dart';
 import 'main.dart';
 
 // * Entry point for the stg flavor
 void main() async {
-  await runMainApp();
+  await runMainApp(firebaseOptions: DefaultFirebaseOptions.currentPlatform);
 }
