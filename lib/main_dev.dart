02260d733c7267b81960cfa30927c38099f08ffe
Andrea Bizzotto <codewithandrea@gmail.com>
2024-11-05 11:43:07 +0000

    Install firebase_core 3.6.0, update settings.gradle and Podfile, initialize Firebase

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,6 +1,7 @@
+import 'firebase_options_dev.dart';
 import 'main.dart';
 
 // * Entry point for the dev flavor
 void main() async {
-  await runMainApp();
+  await runMainApp(firebaseOptions: DefaultFirebaseOptions.currentPlatform);
 }
