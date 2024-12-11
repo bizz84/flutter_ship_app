e03a5f14242798eec9433f4f465af7fc864b5eeb
Andrea Bizzotto <codewithandrea@gmail.com>
2024-12-11 14:18:40 +0000

    Android: update minSdk, targetSdk, add Internet permission

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.codewithandrea.flutter_ship_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
