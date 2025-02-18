430399e662f2ef2ddb642e4635f7329b465696d8
Andrea Bizzotto <codewithandrea@gmail.com>
2025-02-18 14:38:38 +0000

    Cleanup app_colors.dart
    # Conflicts:
    #	lib/src/constants/app_colors.dart

--- lib/src/constants/app_colors.dart
+++ lib/src/constants/app_colors.dart
@@ -3,19 +3,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // https://rydmike.com/flexcolorscheme/themesplayground-latest/
-  // Bahama Blue colors:
-  static const primary = Color(0xFF285C99);
-  static const heart = Color(0xFFDD520F);
-  static const error = Color(0xFFB1384E);
+  static const flexScheme = FlexScheme.bahamaBlue;
 
-  static Color greyShade200(bool isLight) =>
-      isLight ? Colors.grey.shade200 : Colors.grey.shade900;
-  static Color greyShade300(bool isLight) =>
-      isLight ? Colors.grey.shade300 : Colors.grey.shade800;
-  static Color greyShade400(bool isLight) =>
-      isLight ? Colors.grey.shade400 : Colors.grey.shade700;
   static Color greyShade500(bool isLight) =>
       isLight ? Colors.grey.shade500 : Colors.grey.shade600;
-
-  static const flexScheme = FlexScheme.bahamaBlue;
 }
