b106e7dc487c4999be75670462652f9fbe1ddb03
Andrea Bizzotto <codewithandrea@gmail.com>
2025-03-07 11:13:02 +0000

    Add maestro flows

---
+++ maestro/create_apps_ios.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+MAESTRO_LANG=en-GB maestro test -e APP_ID=com.codewithandrea.flutterShipApp.stg create_apps.yaml
---
+++ maestro/take_screenshots_android.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+MAESTRO_LANG=en-GB maestro test -e APP_ID=com.codewithandrea.flutter_ship_app.stg take_screenshots.yaml
