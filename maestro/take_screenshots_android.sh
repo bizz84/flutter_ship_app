c0607fb6a1bcd2579044663a1a3cb4bcc1a43bbb
Andrea Bizzotto <codewithandrea@gmail.com>
2025-03-11 11:08:40 +0000

    Update DEVICE

--- maestro/take_screenshots_android.sh
+++ maestro/take_screenshots_android.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-maestro test -e APP_ID=com.codewithandrea.flutter_ship_app.stg -e LANG=en-GB -e DEVICE=iPhone15Pro take_screenshots.yaml
+maestro test -e APP_ID=com.codewithandrea.flutter_ship_app.stg -e LANG=en-GB -e DEVICE=ANDROID take_screenshots.yaml
--- maestro/take_screenshots_ios.sh
+++ maestro/take_screenshots_ios.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-maestro test -e APP_ID=com.codewithandrea.flutterShipApp.stg -e LANG=en-GB -e DEVICE=iPhone15Pro take_screenshots.yaml
+maestro test -e APP_ID=com.codewithandrea.flutterShipApp.stg -e LANG=en-GB -e DEVICE=IPHONE_65 take_screenshots.yaml
